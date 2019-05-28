30bfad0041d2b1450dbecbf5572fd3f81f0ca7f7
Bartlomiej Mikolajczuk <bartlomiej.mikolajczuk@schibsted.com>
2019-05-28 11:08:48 +0200

    clean some comments

--- main.go
+++ main.go
@@ -1,28 +1,17 @@
-// This is the name of our package
-// Everything with this package name can see everything
-// else inside the same package, regardless of the file they are in
 package main
 
-// These are the libraries we are going to use
-// Both "fmt" and "net" are part of the Go standard library
 import (
-	// "fmt" has methods for formatted I/O operations (like printing to the console)
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 
-	// The "net/http" library has methods to implement HTTP clients and servers
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-// The new router function creates the router and
-// returns it to us. We can now use this function
-// to instantiate and test the router outside of the main function
 func newRouter() *mux.Router {
 	r := mux.NewRouter()
-	r.HandleFunc("/hello", handler).Methods("GET")
 	r.HandleFunc("/flower", getLastWatering).Methods("GET")
 	r.HandleFunc("/flower", createNewWatering).Methods("POST")
 	return r
@@ -84,9 +73,6 @@ func createNewWatering(w http.ResponseWriter, r *http.Request) {
 	w.Write(b)
 }
 
-// "handler" is our handler function. It has to follow the function signature of a ResponseWriter and Request type
-// as the arguments.
 func handler(w http.ResponseWriter, r *http.Request) {
-	// For this case, we will always pipe "Hello World" into the response writer
 	fmt.Fprintf(w, "Hello World!")
 }
